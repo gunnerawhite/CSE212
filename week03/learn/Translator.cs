2276e05907d9b76b3027402dae755523334ddddc
Gunner White <gunnerwhite00@gmail.com>
2024-10-03 11:59:02 -0700

    Homework Wk 3

--- week03/learn/Translator.cs
+++ week03/learn/Translator.cs
@@ -1,30 +1,27 @@
 public class Translator
 {
+    private Dictionary<string, string> _words = new();
+
     public static void Run()
     {
         var englishToGerman = new Translator();
         englishToGerman.AddWord("House", "Haus");
         englishToGerman.AddWord("Car", "Auto");
         englishToGerman.AddWord("Plane", "Flugzeug");
+
         Console.WriteLine(englishToGerman.Translate("Car")); // Auto
         Console.WriteLine(englishToGerman.Translate("Plane")); // Flugzeug
         Console.WriteLine(englishToGerman.Translate("Train")); // ???
     }
 
-    private Dictionary<string, string> _words = new();
-
     /// <summary>
     /// Add the translation from 'from_word' to 'to_word'
-    /// For example, in a english to german dictionary:
-    /// 
-    /// my_translator.AddWord("book","buch")
     /// </summary>
     /// <param name="fromWord">The word to translate from</param>
     /// <param name="toWord">The word to translate to</param>
-    /// <returns>fixed array of divisors</returns>
     public void AddWord(string fromWord, string toWord)
     {
-        // ADD YOUR CODE HERE
+        _words[fromWord] = toWord; // Add or update the translation
     }
 
     /// <summary>
@@ -34,7 +31,7 @@ public class Translator
     /// <returns>The translated word or "???" if no translation is available</returns>
     public string Translate(string fromWord)
     {
-        // ADD YOUR CODE HERE
-        return "";
+        // Try to get the translation; if not found, return "???"
+        return _words.TryGetValue(fromWord, out var toWord) ? toWord : "???";
     }
 }
